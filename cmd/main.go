c1fc3cebd749566ee2c08a35da8d59ffc3b1ba9f
Sujin1135 <mchead1@naver.com>
2025-02-17 19:23:52 +0900

    feat: removed unnecessary logics used for testing

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"channel-contents-collector/api"
-	"channel-contents-collector/pkg/collector"
-	"channel-contents-collector/pkg/external"
-	"fmt"
 )
 
 func main() {
@@ -13,10 +10,4 @@ func main() {
 	if err != nil {
 		return
 	}
-	dataApi := external.NewDataAPI()
-	channelCollector := collector.NewChannelCollector(dataApi)
-	contentCollector := collector.NewContentCollector(dataApi)
-
-	fmt.Println(channelCollector.Collect("뷰티"))
-	fmt.Println(contentCollector.Collect("뷰티"))
 }
